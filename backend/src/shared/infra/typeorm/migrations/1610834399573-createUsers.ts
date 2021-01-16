55f69899cbc0cd0f0103b3575d7bd382a15497fb
Felipe Borges <lipex360@gmail.com>
2021-01-16 19:16:42 -0300

    Migration Users added

---
+++ backend/src/shared/infra/typeorm/migrations/1610834399573-createUsers.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export default class createUsers1610834399573 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'user_id',
+            type: 'uuid',
+            isPrimary: true
+          },
+
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+
+          {
+            name: 'company',
+            type: 'varchar'
+          },
+
+          {
+            name: 'website',
+            type: 'varchar'
+          },
+
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()'
+          },
+
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()'
+          },
+
+          {
+            name: 'deleted_at',
+            type: 'timestamp with time zone',
+            isNullable: true,
+            default: null
+          }
+        ]
+      })
+    )
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
