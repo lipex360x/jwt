35f8c3ce660133d2f63472b8422397863f77d752
Felipe Borges <lipex360@gmail.com>
2021-01-16 18:41:54 -0300

    Server Started

---
+++ backend/src/shared/infra/http/server.ts
@@ -0,0 +1,10 @@
+import app from './app'
+
+const server = app.listen(3333, () => {
+  console.log('API Started on port 3333')
+})
+
+process.on('SIGINT', () => {
+  server.close()
+  console.log('\n🔒 API Stopped')
+})
