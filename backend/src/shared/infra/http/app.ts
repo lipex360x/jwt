57c092b6b475f437a1be5b211721e7ea758493e2
Felipe Borges <lipex360@gmail.com>
2021-01-16 21:25:45 -0300

    AppError added

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -1,14 +1,19 @@
 import express from 'express'
+import 'dotenv/config'
+import 'express-async-errors'
 import 'reflect-metadata'
-
-import routes from './routes'
 
 import '@shared/infra/typeorm'
 import '@shared/containers'
+
+import routes from './routes'
+import appError from '@shared/AppError/RouterError'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use(appError)
 
 export default app
