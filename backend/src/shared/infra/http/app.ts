81e3efdb5223e9ac920553a404318c7465d91100
Felipe Borges <lipex360@gmail.com>
2021-01-17 14:29:23 -0300

    AppError updated

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import { errors } from 'celebrate'
+
 import 'dotenv/config'
 import 'express-async-errors'
 import 'reflect-metadata'
@@ -14,6 +16,7 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+app.use(errors())
 app.use(routerError)
 
 export default app
